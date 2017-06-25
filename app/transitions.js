1fc43959b886889f2252261227b2ac374baab958
Adam Skoczylas <skocadam@gmail.com>
2017-06-25 22:05:02 +0200

    fix header on index add left column on blog page

--- app/transitions.js
+++ app/transitions.js
@@ -1,15 +1,20 @@
-export default function() {
 
-  var duration = 2000;
+let duration = 1000;
 
+export default function(){
   this.transition(
     this.fromRoute('index'),
     this.toRoute('blog'),
-    this.debug(),
-    this.use('explode', {
-      matchBy: '.header-link',
-      use: ['fadeOut', { duration } ]
-    }),
-      this.debug()
+    this.useAndReverse('explode', {
+
+      // Liquid Fire matches elements on data-item-di
+      // And will animate one to the other if the value matches.
+      matchBy: '.test',
+      use: ['flyTo', { duration } ]
+    }, {
+
+      // Everything that doesn't match uses crossFade
+      use: ['crossFade', { duration }]
+    })
   );
 }
